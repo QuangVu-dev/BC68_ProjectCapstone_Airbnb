6df425be512b63811cdffe62c916c8f318ae4df1
QuangVu-dev <tqvu97@gmail.com>
2024-09-28 21:29:33 +0700

    update homepage

--- src/components/ExploreProduct/ExploreProduct.jsx
+++ src/components/ExploreProduct/ExploreProduct.jsx
@@ -26,25 +26,23 @@ const ExploreProduct = () => {
 
   const renderSearchPage = () => {
     return (
-      <div className="max-w-7xl mx-auto sm:px-6 md:px-10 lg:px-20">
-        <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-          {searchPage.map((item, index) => {
-            return (
-              <SmallCard
-                key={item.hinhAnh}
-                hinhAnh={item.hinhAnh}
-                tenViTri={item.tenViTri}
-                tinhThanh={item.tinhThanh}
-              />
-            );
-          })}
-        </section>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        {searchPage.map((item, index) => {
+          return (
+            <SmallCard
+              key={item.hinhAnh}
+              hinhAnh={item.hinhAnh}
+              tenViTri={item.tenViTri}
+              tinhThanh={item.tinhThanh}
+            />
+          );
+        })}
       </div>
     );
   };
 
   return (
-    <section className="py-8">
+    <section className="py-8 max-w-7xl mx-auto sm:px-6 md:px-10 lg:px-20">
       <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
       {renderSearchPage()}
     </section>
