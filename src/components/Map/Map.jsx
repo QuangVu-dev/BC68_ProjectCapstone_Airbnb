22eb8c0910f6f8350c7737037ad77fe2a97d58a2
QuangVu-dev <tqvu97@gmail.com>
2024-09-30 00:45:03 +0700

    update rental room list

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -8,16 +8,21 @@ const Map = () => {
     height: "100%",
     latitude: 10.7769, // Vĩ độ mặc định
     longitude: 106.6951, // Kinh độ mặc định
-    zoom: 10,
+    zoom: 11,
   });
-  const mapboxAccessToken = import.meta.env.VITE_MAPBOX_TOKEN;
-  console.log("Mapbox Access Token:", mapboxAccessToken);
+  const handleViewportChange = (nextViewport) => {
+    setViewport(nextViewport);
+  };
+
   return (
     <ReactMapGL
       {...viewport}
-      mapStyle="mapbox://styles/mapbox/standard"
-      mapboxAccessToken={mapboxAccessToken}
-      onViewportChange={(nextViewport) => setViewport(nextViewport)}
+      mapStyle="mapbox://styles/mapbox/streets-v12"
+      mapboxAccessToken={import.meta.env.VITE_MAPBOX_TOKEN}
+      onMove={handleViewportChange}
+      dragPan={true}
+      scrollZoom={true}
+      doubleClickZoom={true}
     ></ReactMapGL>
   );
 };
--- src/pages/RentalRoomList/RentalRoomList.jsx
+++ src/pages/RentalRoomList/RentalRoomList.jsx
@@ -1,27 +1,25 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import { useSearchParams } from "react-router-dom";
 import { format } from "date-fns";
 import { rentalRoomListService } from "../../services/rentalRoomList.service";
-import InfoCard from "./InfoCard/InfoCard";
+import InfoCard from "../../components/CardBody/InfoCard";
 import Map from "../../components/Map/Map";
 
 const RentalRoomList = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const [rentalRoomList, setRentalRoomList] = useState([]);
-
+  const infoCardRef = useRef(null);
+  const [mapHeight, setMapHeight] = useState(0);
   const selectedLocationId = searchParams.get("tenViTri");
   const checkInDate = searchParams.get("checkIn");
   const checkOutDate = searchParams.get("checkOut");
   const totalGuests = searchParams.get("guests");
   const maViTri = searchParams.get("maViTri");
-  console.log(maViTri);
-  console.log(searchParams);
 
   const formattedCheckInDate = format(new Date(checkInDate), "dd MMMM yy");
   const formattedCheckOutDate = format(new Date(checkOutDate), "dd MMMM yy");
-
   const range = `${formattedCheckInDate} - ${formattedCheckOutDate}`;
 
   useEffect(() => {
@@ -36,18 +34,32 @@ const RentalRoomList = () => {
       });
   }, [maViTri]);
 
+  useEffect(() => {
+    const updateMapHeight = () => {
+      if (infoCardRef.current) {
+        setMapHeight(infoCardRef.current.clientHeight);
+      }
+    };
+    updateMapHeight(); // Cập nhật ngay sau khi render
+    window.addEventListener("resize", updateMapHeight); // Cập nhật khi resize
+    return () => {
+      window.removeEventListener("resize", updateMapHeight); // Dọn dẹp
+    };
+  }, [rentalRoomList]);
+
   return (
     <div>
       <Header />
-      <main className="flex mt-20">
-        <section className="max-w-7xl sm:px-6 md:px-10 lg:px-20">
-          <p className="text-xs">
-            300+ Stays - {range} - for {totalGuests} guests
+      <main className="flex mt-20 max-w-7xl sm:px-6 md:px-10 lg:px-20">
+        <section ref={infoCardRef} className="pr-5 sm:w-full lg:w-1/2 xl:w-2/3">
+          <p className="text-base">
+            300+ Stays - {range} - for {totalGuests} {""}
+            {totalGuests === "1" ? "guest" : "guests"}
           </p>
           <h1 className="text-3xl font-semibold mt-2 mb-6">
             Stays in {selectedLocationId}
           </h1>
-          <div className="hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap">
+          <div className="hidden lg:inline-flex lg:text-base lg:flex-wrap xl:flex-nowrap mb-5 space-x-3 text-gray-800 whitespace-nowrap mr-5">
             <p className="button">Cancellation Flexibility</p>
             <p className="button">Type of Place</p>
             <p className="button">Price</p>
@@ -67,7 +79,10 @@ const RentalRoomList = () => {
           })}
         </section>
 
-        <section className="hiiden xl:inline-flex xl:min-w-[600px]">
+        <section
+          className="hidden lg:inline-flex lg:w-1/2 xl:w-1/3 flex-grow overflow-hidden"
+          style={{ height: mapHeight }}
+        >
           <Map />
         </section>
       </main>
